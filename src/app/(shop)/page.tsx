2903f1b940ab08b2270a74b3ecdd320bf7997f73
ernestovv85 <ernestovv85@gmail.com>
2024-07-11 14:02:33 -0600

    Add menu and products list

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,14 @@
-import { titleFont } from "@/config/fonts";
+import { ProductGrid, Title } from '@/components';
+import { initialData } from '@/seed/seed';
+
+const products = initialData.products;
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Hello World</h1>
-      <h1 className={`${titleFont.className} font-bold`}>Hello World</h1>
-    </div>
+    <>
+      <Title title="Tienda" subtitle="Todos los productos" />
+
+      <ProductGrid products={products} />
+    </>
   );
 }
