c498f3b61318674158d83e83079bc2518b670cd7
ernestovv85 <ernestovv85@gmail.com>
2024-07-12 10:34:55 -0600

    Filter for gender added

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,20 +1,39 @@
-import { notFound } from "next/navigation";
+import { ProductGrid, Title } from '@/components';
+import { Gender } from '@/interfaces';
+import { initialData } from '@/seed/seed';
+import { notFound } from 'next/navigation';
+
+const seedProducts = initialData.products;
 
 interface Props {
   params: {
-    id: string
-  }
+    id: Gender;
+  };
 }
-export default function CategoryPage({params}: Props) {
+export default function CategoryPage({ params }: Props) {
   const { id } = params;
+  const products = seedProducts.filter((product) => product.gender === id);
 
-  if (id !== 'kids' && id !== 'women' && id !== 'men') {
+  const labels: Record<Gender, string> = {
+    men: 'Hombres',
+    women: 'Mujeres',
+    kid: 'Niños',
+    unisex: 'Unisex',
+  };
+
+  if (id !== 'kid' && id !== 'women' && id !== 'men') {
     notFound();
   }
 
   return (
-    <div>
-      <h1>Category Page {id}</h1>
-    </div>
+    <>
+      <Title
+        title={`Artículos para ${labels[id]}`}
+        subtitle="Todos los productos"
+        className="mb-2"
+      />
+
+      <ProductGrid products={products} />
+    </>
   );
 }
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -6,7 +6,7 @@ const products = initialData.products;
 export default function Home() {
   return (
     <>
-      <Title title="Tienda" subtitle="Todos los productos" />
+      <Title title="Tienda" subtitle="Todos los productos" className='mb-2'/>
 
       <ProductGrid products={products} />
     </>
