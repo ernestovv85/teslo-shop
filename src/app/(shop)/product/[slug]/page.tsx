826396cbc1f07624bf37761741b2307804283933
ernestovv85 <ernestovv85@gmail.com>
2024-07-12 16:09:22 -0600

    Slideshow added in product detail

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,17 +1,16 @@
-import { QuantitySelector, SizeSelector } from "@/components";
-import { titleFont } from "@/config/fonts";
-import { initialData } from "@/seed/seed";
-import { notFound } from "next/navigation";
-
+import { MobileSlideshow, QuantitySelector, SizeSelector, Slideshow } from '@/components';
+import { titleFont } from '@/config/fonts';
+import { initialData } from '@/seed/seed';
+import { notFound } from 'next/navigation';
 
 interface Props {
   params: {
     slug: string;
-  }
+  };
 }
-export default function ProductPage({params}: Props) {
+export default function ProductPage({ params }: Props) {
   const { slug } = params;
-  const product = initialData.products.find(product => product.slug === slug);
+  const product = initialData.products.find((product) => product.slug === slug);
 
   if (!product) {
     notFound();
@@ -21,22 +20,33 @@ export default function ProductPage({params}: Props) {
     <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
       {/* Slideshow */}
       <div className="col-span-1 md:col-span-2">
+        {/* Desktop slideshow */}
+        <Slideshow title={product.title} images={product.images} className='hidden md:block'/>
 
+        {/* Mobile slideshow */}
+        <MobileSlideshow title={product.title} images={product.images} className='block md:hidden'/>
       </div>
       {/* Product Info */}
       <div className="col-span-1 px-5">
-        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>{product.title}</h1>
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
         <p className="text-lg mb-5">${product.price}</p>
 
         {/* Size selector */}
-        <SizeSelector selectedSize={product.sizes[0]} availableSizes={product.sizes}/>
+        <SizeSelector
+          selectedSize={product.sizes[0]}
+          availableSizes={product.sizes}
+        />
         {/* Quantity selector */}
-        <QuantitySelector quantity={1}/>
+        <QuantitySelector quantity={1} />
         {/* Add to cart */}
         <button className="btn-primary my-5">Agregar al carrito</button>
 
         {/* Description */}
-        <h3 className="font-bold text-sm"> Descripción: 
+        <h3 className="font-bold text-sm">
+          {' '}
+          Descripción:
           <p className="font-light">{product.description}</p>
         </h3>
       </div>
